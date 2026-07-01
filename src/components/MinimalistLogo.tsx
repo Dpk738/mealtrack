3673f7992516a81efd0478937807b183b96357e9
Deepak Srinivas <deepakbnivas882@gmail.com>
2026-07-01 19:23:13 +0530

    design: Change logo and favicon to minimalist black & radium flame (fat-burning/streak) icon

--- src/components/MinimalistLogo.tsx
+++ src/components/MinimalistLogo.tsx
@@ -18,10 +18,11 @@ export default function MinimalistLogo({ size = 24, showText = true }: Minimalis
       >
         {/* Radium color box background */}
         <rect width="24" height="24" rx="6" fill="#cbf600" />
-        {/* Minimalist black circle cutout */}
-        <circle cx="12" cy="12" r="5" fill="#000000" />
-        {/* Central radium dot */}
-        <circle cx="12" cy="12" r="2.5" fill="#cbf600" />
+        {/* Minimalist black flame cutout representing fat burning / streak */}
+        <path
+          d="M8.5 14.5A2.5 2.5 0 0 0 11 12c0-1.38-.5-2-1-3-1.072-2.143-.224-4.054 2-6 .5 2.5 2 4.9 4 6.5 2 1.6 3 3.5 3 5.5a7 7 0 1 1-14 0c0-1.153.433-2.294 1-3a2.5 2.5 0 0 0 2.5 2.5z"
+          fill="#000000"
+        />
       </svg>
       {showText && (
         <span
